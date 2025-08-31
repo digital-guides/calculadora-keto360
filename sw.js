fe6036f34b428f05c6aa2fa6a8b5e04bf4c32e24
digital-guides <chicalalala@gmail.com>
2025-08-31 14:35:37 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,32 @@
+// Service Worker básico para PWA
+const CACHE_NAME = 'calculadora-keto-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/calculadora.html',
+  '/guia.html',
+  '/manifesto.webmanifest'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Cache abierto');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
